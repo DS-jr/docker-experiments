0a7e5cb7d17040d48b3260be59a4521f96049b41
DS-jr <dmitry.k.sokolov@gmail.com>
2022-11-13 20:27:46 +0300

    build images. containerize an app; start; update; share

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">nO itEms yEt, aDd oNe aBoVe. be boLD !</p>
             )}
             {items.map(item => (
                 <ItemDisplay
